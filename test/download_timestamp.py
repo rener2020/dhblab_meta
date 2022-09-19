13342e51774be7793d29d3948c7a5cd1c49df7f2
rener2020 <1295606692@qq.com>
2022-09-19 12:20:45 +0800

    time delay test

--- test/download_timestamp.py
+++ test/download_timestamp.py
@@ -1,10 +1,8 @@
 from time import time
-
-
 import time
 
 while True:
-    with open("../send_cache.txt", 'r') as f:
+    with open("recv_cache.txt", 'r') as f:
         print("pre",f.read())
         print("cur",time.time())
     time.sleep(0.01)
--- test/upload_timestamp.py
+++ test/upload_timestamp.py
@@ -4,6 +4,6 @@ from time import time
 import time
 
 while True:
-    with open("../send_cache.txt", 'w') as f:
+    with open("send_cache.txt", 'w') as f:
         f.write(str(time.time()))
     time.sleep(0.01)
